ed754fcf9cf070f9658d018cdf20ddc46349a6a3
michail.pa.2003 <michail.pa.2003>
2025-07-11 18:19:22 +0200

    update genres

--- games_hub/game/views.py
+++ games_hub/game/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Game
+from .models import Game, GENRE_CHOICES
+from django.db import models
 # Create your views here.
 def home(request):
     games = Game.objects.all()
@@ -7,7 +8,21 @@ def home(request):
     searched_word = request.GET.get('search')
     if searched_word:
         games = games.filter(title__contains=searched_word)
-    return render(request, 'base.html', {'games': games})
+
+    selected_genre = request.GET.get('genre')
+    if selected_genre:
+        games = games.filter(
+            models.Q(genre=selected_genre) |
+            models.Q(genre2=selected_genre) |
+            models.Q(genre3=selected_genre)
+        )
+
+    genres = GENRE_CHOICES  # Pass value and display name
+    return render(request, 'base.html', {
+        'games': games,
+        'genres': genres,
+        'selected_genre': selected_genre,
+    })
 
 def game_detail(request, pk):
     game = get_object_or_404(Game, pk=pk)
